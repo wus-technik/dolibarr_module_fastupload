1ebc923a5131e7fef4b2e9793681dedd249cc20e
jpb <jean-pascal.boudet@atm-consulting>
2021-10-26 12:13:49 +0200

    add token

---
+++ script/interface.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('NOTOKENRENEWAL')) define('NOTOKENRENEWAL', 1); // Disables token renewal
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
